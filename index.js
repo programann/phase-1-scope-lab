b4b9314f332d4d92dd4665bb1e461413d46f8b20
Clarence-Kinuthia <mclarence7311@gmail.com>
2024-03-21 09:13:47 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    console.log(customerName)
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavouriteCustomer = "Kamau"
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer = "karimi"
+}
